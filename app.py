140e323a873f9746f44236f41d2662ba4f8f994e
nsl1011 <stjopka_nl@edu.surgu.ru>
2023-01-16 20:53:57 +0500

    1.1

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template, url_for
+#import sentry_sdk
+# from sentry_sdk.integrations.flask import FlaskIntegration
+
+# sentry_sdk.init(
+#     dsn="https://edf95c656a5644c08e7f95753ede281a@o4504514952101888.ingest.sentry.io/4504514955313152",
+#     integrations=[
+#         FlaskIntegration(),
+#     ],
+
+#     traces_sample_rate=1.0
+# )
 
 app = Flask (__name__)
 
